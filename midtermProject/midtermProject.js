bcdd674e4daccb2cb38478651b8d14af2f6ae1dd
MishelRashid <98512494+MishelRashid@users.noreply.github.com>
2022-03-10 04:57:29 +0400

    Update midtermProject.js

--- midtermProject/midtermProject.js
+++ midtermProject/midtermProject.js
@@ -1,3 +1,7 @@
+//Mishel Rashid
+//10 March 2022
+//Flying Dpg
+
 let dogImg; //variable that will hold the image of the dog
 let bgImg; //variable that will hold the background image
 let cloudImg; //variable that will hold the image of the cloud
